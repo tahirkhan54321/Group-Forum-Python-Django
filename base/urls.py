69d02890b2cb6c06bfb6f025b21dc08eaec9917a
Tahir Khan <tahir_khan54321@hotmail.com>
2024-05-29 12:16:29 +0100

    Added create room button

--- base/urls.py
+++ base/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('room/<str:pk>/', views.room, name="room"), #pk stands for primary key
+    path('create-room/', views.createRoom, name="create-room"),
 ]
