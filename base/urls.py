eb3903ac3848cbf009180129a56611436de9da32
Tahir Khan <tahir_khan54321@hotmail.com>
2024-05-29 14:55:32 +0100

    Added delete room functionality

--- base/urls.py
+++ base/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('room/<str:pk>/', views.room, name="room"), #pk stands for primary key
     path('create-room/', views.createRoom, name="create-room"),
     path('update-room/<str:pk>/', views.updateRoom, name="update-room"),
+    path('delete-room/<str:pk>/', views.deleteRoom, name="delete-room"),
 ]
