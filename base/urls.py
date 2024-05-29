47e2ec692eff52b0c70b01737e73d5468cd48e82
Tahir Khan <tahir_khan54321@hotmail.com>
2024-05-29 18:31:43 +0100

    Added login and logout functionality

--- base/urls.py
+++ base/urls.py
@@ -6,6 +6,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
     path('', views.home, name="home"),
     path('room/<str:pk>/', views.room, name="room"), #pk stands for primary key
     path('create-room/', views.createRoom, name="create-room"),
