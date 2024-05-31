604dea42963cb677d7366deeb1ad0799044dc6fa
Tahir Khan <tahir_khan54321@hotmail.com>
2024-05-31 12:35:15 +0100

    Room form refactored and Topics updated accordingly

--- base/views.py
+++ base/views.py
@@ -111,31 +111,41 @@ def userProfile(request, pk):
 @login_required(login_url='login')   # a decorator from the imports which restricts this page if a user is not logged in
 def createRoom(request):
     form = RoomForm()
+    topics = Topic.objects.all()
     if request.method == 'POST':
-        form = RoomForm(request.POST)
-        if form.is_valid():
-            room = form.save(commit=False)
-            room.host = request.user
-            room.save()
-            return redirect('home')
-    context = {'form': form}
+        
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name) # either post to the existing topic or create a new one
+        
+        Room.objects.create(
+            host=request.user,
+            topic=topic,
+            name=request.POST.get('name'),
+            description=request.POST.get('description')
+        )
+        return redirect('home')
+    context = {'form': form, 'topics': topics,}
     return render(request, 'base/room_form.html', context)
 
 @login_required(login_url='login')   # a decorator from the imports which restricts this page if a user is not logged in
 def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
+    topics = Topic.objects.all()
 
     if request.user != room.host:
         return HttpResponse('You are not allowed here!')
 
     if request.method == 'POST':
-        form = RoomForm(request.POST, instance=room) # instance replacing data rather than adding a new room
-        if form.is_valid():
-            form.save()
-            return redirect('home')
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name) # either post to the existing topic or create a new one
+        room.name = request.POST.get('name')
+        room.topic = topic
+        room.description = request.POST.get('description')
+        room.save()
+        return redirect('home')
         
-    context = {'form': form}
+    context = {'form': form, 'topics': topics, 'room': room}
     return render(request, 'base/room_form.html', context)
 
 @login_required(login_url='login')   # a decorator from the imports which restricts this page if a user is not logged in
