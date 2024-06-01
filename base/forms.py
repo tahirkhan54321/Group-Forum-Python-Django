9e7f84ba2a12efb0795adaae6eea79447bdc67be
Tahir Khan <tahir_khan54321@hotmail.com>
2024-06-01 21:47:18 +0100

    Edit profile functionality added

--- base/forms.py
+++ base/forms.py
@@ -2,6 +2,7 @@
 
 from django.forms import ModelForm
 from .models import Room
+from django.contrib.auth.models import User
 
 class RoomForm(ModelForm):
     class Meta:
@@ -9,3 +10,8 @@ class RoomForm(ModelForm):
         # fields will take all of the fields in the Room model and create a form for each
         fields = '__all__'
         exclude = ['host', 'participants']
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
